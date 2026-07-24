add9e151ed6441f16b595c868177c2498a1a4a53
Monika.L <monikalokesh4@gmail.com>
2026-07-24 23:37:10 +0530

    Time: 5 ms (13.07%) | Memory: 22.4 MB (66.57%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.py
+++ 203-remove-linked-list-elements/remove-linked-list-elements.py
@@ -1,13 +1,12 @@
 class Solution:
-  def removeElements(self, head: ListNode, val: int) -> ListNode:
-    dummy = ListNode(0, head)
-    prev = dummy
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        curr = dummy
 
-    while head:
-      if head.val != val:
-        prev.next = head
-        prev = prev.next
-      head = head.next
-    prev.next = None
+        while curr.next:
+            if curr.next.val == val:
+                curr.next = curr.next.next
+                continue
+            curr = curr.next
 
-    return dummy.next
+        return dummy.next
